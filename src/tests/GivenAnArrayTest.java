660aa66d5fa83f4a5b14aa4d0c755dbee29a4d04
Priya Khodwe <84349071+psk264@users.noreply.github.com>
2023-01-09 10:49:02 -0500

    update to src and test folders

--- src/tests/GivenAnArrayTest.java
+++ src/tests/GivenAnArrayTest.java
@@ -1,6 +1,11 @@
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GivenAnArrayTest {
 
